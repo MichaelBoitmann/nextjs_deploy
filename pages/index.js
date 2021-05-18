93e7d6e747c89414a1e6987d70c42a8f049891ee
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2021-05-18 20:37:36 +1200

    basic of NextJS

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Layout from '../src/components/layout';
+import Posts from '../src/components/home/posts';
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>Create NextJS App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Posts />
+    </Layout>
+  )
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // @see https://tailwindcss.com/docs/upcoming-changes
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './src/components/**/*.js',
+    './pages/**/*.js'],
+  theme: {
+    minHeight: {
+      '60vh': "60vh"
+    }
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('precss'),
+    require('autoprefixer')
+  ]
+}
